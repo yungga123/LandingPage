b5c570b4221433d9a6342601c731f2c14e661d9b
yungga123 <53594401+yungga123@users.noreply.github.com>
2021-10-04 16:08:08 +0800

    last update

--- application/views/landingpage/landingpage.php
+++ application/views/landingpage/landingpage.php
@@ -1,19 +1,32 @@
 <style>
-    .card-img-top {
+   .card-img-top {
         width: 100%;
-        max-width: 50px;
-        height: 9%;
-        border-radius: 10%;
+        max-width: 100px;
+        height: 90px;
+        border: solid;
+        color: white;
+        margin: 2%;
+        border-radius: 3%;
     }
-    #page {
+    .page-description {
         height: 15%;
     }
     .paragraph {
-       padding-top: 5%
+       padding-top: 50%;
     }
     .ads {
         margin: 1%;
-        padding-top: 5%;
+        padding-top: 10%;
+    }
+    .my-auto {
+        border: solid;
+        background-color: rgba(0, 0, 0, 0.5);
+        text-align: center;
+        width: 60%;
+        max-width: 70%;
+        height: auto;
+        max-height: 2000px;
+        margin: auto;
     }
 </style>
 
@@ -29,7 +42,6 @@
     <link rel="stylesheet" href="<?php echo base_url('assets/materialdesign/css/materialdesignicons.min.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/css/bd-coming-soon.css') ?>">
-    <link href="css/styles.css" rel="stylesheet" />
 </head>
 
 <body class="min-vh-100 d-flex flex-column">
@@ -48,26 +60,26 @@
     <main class="my-auto">
         <div class="container">
             <h1 class="page-title">Welcome to Brgy. Poblacion</h1>
-            <p class="page-description" id="page">Inspired by the Philippine’s ‘Bahay Kubo’, designed by Architect Roderick Espina, it houses the head of the city, the executive, legislative and administrative offices of the city.The City Hall of Muntinlupa is home to our Single-Window Transaction, an award-winning one-stop shop mechanism for business stakeholders.
-            </p>
+            <center><p class="page-description">Inspired by the Philippine’s ‘Bahay Kubo’, designed by Architect Roderick Espina, it houses the head of the city, the executive, legislative and administrative offices of the city.<br><br>The City Hall of Muntinlupa is home to our Single-Window Transaction, an award-winning one-stop shop mechanism for business stakeholders.
+            </p></center>
             <div class="paragraph">
             <p>Powered by</p>
             </div>
             <div class="ads">
                 <a href="https://vinculumtechnologies.com/?fbclid=IwAR3DrNnlBVOdwDrwPMXHHHAIUrxcQOIrku-GdBDQKMd80M2JAFdG1-sGosQ">
-                <img class="card-img-top" src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.6435-9/42510149_2139131869451698_2550321017377521664_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=174925&_nc_eui2=AeFsNRyfDvQeNGdpJKYTzuQloIQqfaIH0J6ghCp9ogfQnigU1FoDQExuju_wWvbp9VZjk3BaP_5XxO1FtbaQFi_f&_nc_ohc=xSFdcCahKR4AX9-kqes&_nc_ht=scontent.fmnl17-2.fna&oh=caf9c9ad347df92723d60f74d56e5c62&oe=6175C94C" alt="..." />
+                <img class="card-img-top" src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.6435-9/120181492_3691107720920764_8342186659661035570_n.jpg?_nc_cat=107&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeF6hqw5BIfv4orhNDVd7v8_itVXI3k6cmSK1VcjeTpyZEBh5Huk_IXT7xEeHhOkPWnfZ9XVybpVlHHUGblwQdYC&_nc_ohc=gdQ0HwCclWMAX9VrRQv&_nc_ht=scontent.fmnl17-2.fna&oh=fa58eef81e7725b39f24f976cc05da4a&oe=617BA715" />
                 </a>
                 <a href="https://www.facebook.com/CityPantherInfoTechCorp/">
-                <img class="card-img-top" src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.6435-9/173429922_516260659757498_2350304113593636471_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeEFtwM5v-PoJU8VkLyxAlIeC-hO6SA1wJoL6E7pIDXAmtLpNil28MtTrM9YfpQx_-mDAWS_kjBu3DR4Td7sMKJO&_nc_ohc=THgeYPZ7OmUAX_oJAq_&_nc_ht=scontent.fmnl17-2.fna&oh=a8994d2c9677ae235251a4cde6b18b27&oe=61769352" alt="..." />
+                <img class="card-img-top" src="https://scontent.fmnl17-2.fna.fbcdn.net/v/t1.6435-9/173429922_516260659757498_2350304113593636471_n.jpg?_nc_cat=109&ccb=1-5&_nc_sid=e3f864&_nc_eui2=AeEFtwM5v-PoJU8VkLyxAlIeC-hO6SA1wJoL6E7pIDXAmtLpNil28MtTrM9YfpQx_-mDAWS_kjBu3DR4Td7sMKJO&_nc_ohc=THgeYPZ7OmUAX_oJAq_&_nc_ht=scontent.fmnl17-2.fna&oh=a8994d2c9677ae235251a4cde6b18b27&oe=61769352" />
                 </a>
                 <a href="https://www.jollibee.com.ph/">
-                <img class="card-img-top" src="https://sa.kapamilya.com/absnews/abscbnnews/media/2015/business/11/02/jollibee-logo.jpg" alt="..." />
+                <img class="card-img-top" src="https://sa.kapamilya.com/absnews/abscbnnews/media/2015/business/11/02/jollibee-logo.jpg" />
                 </a>
                 <a href="https://www.mcdonalds.com.ph/">
-                <img class="card-img-top" src="https://th.bing.com/th/id/R.0c3091d8c50e0b179e42c44c5fc75d44?rik=WoMledMLOavXNQ&riu=http%3a%2f%2f2.bp.blogspot.com%2f-2mpvyKBYsRg%2fTsOYqyiR70I%2fAAAAAAAAElw%2fdMimBFyVXI8%2fs1600%2fMcDonalds%2bLogo.jpg&ehk=e%2f60vs12oOyzMXv4SR9xF5IPDwSSI%2bQk6SgQSv5nf6o%3d&risl=&pid=ImgRaw&r=0" alt="..." />
+                <img class="card-img-top" src="https://th.bing.com/th/id/R.0c3091d8c50e0b179e42c44c5fc75d44?rik=WoMledMLOavXNQ&riu=http%3a%2f%2f2.bp.blogspot.com%2f-2mpvyKBYsRg%2fTsOYqyiR70I%2fAAAAAAAAElw%2fdMimBFyVXI8%2fs1600%2fMcDonalds%2bLogo.jpg&ehk=e%2f60vs12oOyzMXv4SR9xF5IPDwSSI%2bQk6SgQSv5nf6o%3d&risl=&pid=ImgRaw&r=0" />
                 </a>
                 <a href="https://www.skype.com/en/">
-                <img class="card-img-top" src="https://th.bing.com/th/id/R.80ae3c9da286d1f75254c4251a24099c?rik=HosDKa2HGQYuNw&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f06%2fFont-Skype-Logo.jpg&ehk=A2RcZJ%2bVek9XOsjQmmKHsh7sB1oUu1PLj775LVa8h54%3d&risl=&pid=ImgRaw&r=0" alt="..." />
+                <img class="card-img-top" src="https://th.bing.com/th/id/R.80ae3c9da286d1f75254c4251a24099c?rik=HosDKa2HGQYuNw&riu=http%3a%2f%2f1000logos.net%2fwp-content%2fuploads%2f2017%2f06%2fFont-Skype-Logo.jpg&ehk=A2RcZJ%2bVek9XOsjQmmKHsh7sB1oUu1PLj775LVa8h54%3d&risl=&pid=ImgRaw&r=0" />
                 </a>
             </div>
         </div>
